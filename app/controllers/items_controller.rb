d913eff1b45c41ab1f3e579a858441bcba4c58a4
aiuton <71863686+aiuton@users.noreply.github.com>
2020-11-07 20:04:54 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,11 +16,27 @@ class ItemsController < ApplicationController
     else
       render :new
     end
-  end
+  end  
 
   def show
     @item = Item.find(params[:id])
   end
+
+  def edit
+    @item = Item.find(params[:id])
+    unless current_user == @item.user
+      redirect_to root_path
+    end
+    end
+  
+    def update
+      @item = Item.find(params[:id])
+      if @item.update(item_params)
+        redirect_to item_path(@item)
+      else
+        render :edit
+      end
+    end
 
   private
 
